b27c18c5cfbf8efba186a84d6319c3a9a9270d7f
Jorge Villalobos <jorgemvc@gmail.com>
2017-06-18 08:59:19 -0600

    Update SensorRGB.ino

--- SensorRGB.ino
+++ SensorRGB.ino
@@ -9,6 +9,7 @@
 
 // ---- Variables
 SensorRGB rgb;
+// Matriz con la deficion RGB de los colores
 uint16_t defColores[7][3] = {
   {255, 0  , 0  },
   {0  , 255, 0  },
@@ -18,6 +19,7 @@ uint16_t defColores[7][3] = {
   {255, 255, 255},
   {255, 178, 102}
 };
+// Nombre correspondiente de los colores definidos
 String colores[7] = {
   "Rojo",
   "Verde",
@@ -30,7 +32,7 @@ String colores[7] = {
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("Robot con sensor ultrasonico");
+  Serial.println("Sensor RGB");
   Serial.println("Jorge Villalobos C. - v1.0 - Jun.2017");
   Serial.println("");
 
@@ -39,9 +41,14 @@ void setup() {
 }  // void setup();
 
 void loop() {
+  // Realiza la lectura
   rgb.getRGBColor();
   Serial.print("Color detectado: ");
+  
+  // Determina por aproximacion el color
   Serial.print(getColor());
+  
+  // Despliega la composicion RGB del color detectado
   Serial.print(" | R:"); Serial.print(rgb.Red());
   Serial.print(" - G:"); Serial.print(rgb.Green());
   Serial.print(" - B:"); Serial.print(rgb.Blue());
@@ -54,6 +61,9 @@ String getColor() {
   int dist = 1000,
       aux;
   
+  // Calcula la distancia entre el color detectado y los
+  // diferentes colores definidos, el que tenga menor distancia
+  // sera el elegido.
   for (int i = 0 ; i < 7 ; i++) {
     aux = rgb.Diff(defColores[i][0], defColores[i][1], defColores[i][2]);
     if (aux < dist) {
@@ -64,4 +74,3 @@ String getColor() {
 
   return colores[indice];
 }  // String GetColor()
-
